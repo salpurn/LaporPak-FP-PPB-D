1b1e655f5c2a4474434f79a95523c19b5618afa4
athallaabhi <athallaabhi@gmail.com>
2026-06-05 02:14:25 +0700

    chore: remove dead auth_service stub and unused home_page

--- lib/core/services/auth_service.dart
+++
@@ -1,8 +0,0 @@
-import '../models/app_user.dart';
-
-abstract class AuthService {
-  Future<AppUser> signIn({required String email, required String password});
-  Future<void> signOut();
-  AppUser? currentUser();
-  Stream<AppUser?> watchAuthState();
-}
